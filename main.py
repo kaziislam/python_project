7556e62639f7f83bcc2357ad64d48e3dc6e5a862
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2023-08-12 12:22:27 -0500

    exercise#1 of automation

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-# import helper
-from helper import validate_and_execute, user_input_message
-import os
+import openpyxl
 
-print(os.cpu_count())
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
 
-""" user_input = ""
-while user_input != "exit":
-    user_input = input(user_input_message)
-    days_and_unit = user_input.split(":")
-    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    # helper.validate_and_execute(days_and_unit_dictionary)
-    validate_and_execute(days_and_unit_dictionary) """
+# dictionaries
+products_per_supplier = {}
+total_value_per_supplier = {}
 
+# this will print total rows
+print(product_list.max_row)
 
+# this line will start from line 2 as first line is header and we do not want this data
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+
+    # calculation for number of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_of_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_of_products + 1
+    else:
+        print("adding new supplier")
+        products_per_supplier[supplier_name] = 1
+    # print(products_per_supplier)
+
+    # calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+print(products_per_supplier)
+print(total_value_per_supplier)
