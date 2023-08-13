00f167c94af1f9ec3d1fa8e81195cff6f02c1fda
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2023-08-13 12:28:34 -0500

    class object/methods

--- main.py
+++ main.py
@@ -1,55 +1,17 @@
-import openpyxl
+#import user
+from user import User
+from post import Post
 
-inv_file = openpyxl.load_workbook("inventory.xlsx")
-product_list = inv_file["Sheet1"]
+# create an object from class
+app_user_one = User("joe@email.com", "Joe Smith", "Passw0rd", "Software Engineer")
+app_user_one.get_user_info()
 
-# dictionaries
-products_per_supplier = {}
-total_value_per_supplier = {}
-# dictionary for product under 20
-products_under_20_inv = {}
+app_user_one = User("jane@email.com", "Jane Doe", "Passw0rd", "DevOps Engineer")
+app_user_one.get_user_info()
 
-# this will print total rows
-# print(product_list.max_row)
 
-# this line will start from line 2 as first line is header and we do not want this data
-for product_row in range(2, product_list.max_row + 1):
-    supplier_name = product_list.cell(product_row, 4).value
-    inventory = product_list.cell(product_row, 2).value
-    price = product_list.cell(product_row, 3).value
-    product_num = product_list.cell(product_row, 1).value
-    inventory_price = product_list.cell(product_row, 5)
+app_user_one.change_job_title("DevOps Engineer")
+app_user_one.get_user_info()
 
-    # calculation for number of products per supplier
-    if supplier_name in products_per_supplier:
-        current_num_of_products = products_per_supplier.get(supplier_name)
-        products_per_supplier[supplier_name] = current_num_of_products + 1
-    else:
-        # print("adding new supplier")
-        products_per_supplier[supplier_name] = 1
-    # print(products_per_supplier)
-
-    # calculation total value of inventory per supplier
-    if supplier_name in total_value_per_supplier:
-        current_total_value = total_value_per_supplier.get(supplier_name)
-        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
-    else:
-        total_value_per_supplier[supplier_name] = inventory * price
-
-# print(products_per_supplier)
-# print(total_value_per_supplier)
-
-    # logic for products inventory under 10
-    if inventory < 20:
-        products_under_20_inv[product_num] = inventory
-
-# print(products_under_20_inv)
-
-    #add value for total inventory price
-    inventory_price.value = inventory * price
-
-print(products_per_supplier)
-print(total_value_per_supplier)
-print(products_under_20_inv)
-
-inv_file.save("inventory_with_total_value.xlsx")
+new_post = Post("Learning Python", app_user_one.name)
+new_post.get_post_info()
---
+++ post.py
@@ -0,0 +1,8 @@
+class Post:
+    def __init__(self, message, author):
+        self.message = message
+        self.author = author
+
+    def get_post_info(self):
+        print(f"Post: {self.message} written by {self.author}")
+
