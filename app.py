61af7fd5677171185629673b3f7f3c1a6b66649c
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2023-08-17 17:48:50 -0500

    webpage working as planned

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def get_projects():
-    response = requests.get("https://api.github.com/users/kaziislam/repos")
-    my_projects = response.json()
-    return render_template('projects.html', projects=my_projects)
+    if request.method == 'POST':
+        username = request.form['username']
+        response = requests.get(f"https://api.github.com/users/{username}/repos")
+        my_projects = response.json()
+        return render_template('projects.html', projects=my_projects, username=username)
+    return render_template('index.html', projects=[])
 
 
 if __name__ == '__main__':
